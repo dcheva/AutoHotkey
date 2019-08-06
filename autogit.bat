c5522e6d5737295a6e8dbd1e45838acfedbd7fc8
Dmitry Cheva <dmitry.cheva@gmail.com>
2019-08-06 08:09:27 +0300

    loop 100 autogit bat

--- autogit.bat
+++ autogit.bat
@@ -7,8 +7,10 @@ git push origin HEAD
 IF /I "%1" == "loop" (
   IF /I "%2" GEQ "100" (
       git ls-files -m > git.log
-      type git.log
-      set /p gitlog=<git.log
+      set gitlog = ""
+      for /F "delims=" %%a in git.log do (
+        set gitlog =<< "%%a "
+      )
       IF /I "%gitlog%" NEQ "" (
       timeout /t "%2"
       git add -A
