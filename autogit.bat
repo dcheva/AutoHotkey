9d4d8b252654cd27ee3dff1afbfff28783067e4f
Dmitry Cheva <dmitry.cheva@gmail.com>
2019-08-06 08:49:46 +0300

    loop autogit up

--- autogit.bat
+++ autogit.bat
@@ -5,10 +5,12 @@ git pull
 git push origin HEAD
 :loop
 IF /I "%1" == "loop" (
+    set t = %2
+    IF /I %t% LEQ 60 do (set t=60)
     git add -A
     git commit -am "%datetimef%"
     git pull
     git push origin HEAD
-    timeout /t "%2"
+    timeout /t %t%
     goto loop
 )
