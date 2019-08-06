c769c66ce3802dfccf110806e393d0c153c6ce6b
Dmitry Cheva <dmitry.cheva@gmail.com>
2019-08-06 08:05:19 +0300

    loop 100 autogit bat

--- autogit.bat
+++ autogit.bat
@@ -7,6 +7,7 @@ git push origin HEAD
 IF /I "%1" == "loop" (
   IF /I "%2" GEQ "100" (
       git ls-files -m > git.log
+      type git.log
       set /p gitlog=<git.log
       IF /I "%gitlog%" NEQ "" (
       timeout /t "%2"
