1768267f0fd4c304fa2814070a0a4ab86dbf683a
Dmitry Cheva <dmitry.cheva@gmail.com>
2019-08-06 07:34:27 +0300

    loop 100 autogit bat

--- autogit.bat
+++ autogit.bat
@@ -3,11 +3,13 @@ git commit -am "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 git pull
 git push origin HEAD
 :loop
-IF /I "%1" == "loop" && "$2" GEQ "100" (
-	timeout /t "%2"
-	git add -A
-	git commit -am "%date% %time%"
-	git pull
-	git push origin HEAD
-	goto loop
+IF /I "%1" == "loop"  (
+  IF /I "$2" GEQ "100" (
+    timeout /t "%2"
+    git add -A
+    git commit -am "%date% %time%"
+    git pull
+    git push origin HEAD
+    goto loop
+  )
 )
