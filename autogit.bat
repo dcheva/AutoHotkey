04b7fff65b8a69bef1efbb89d3f3331d94a9dd76
Dmitry Cheva <dmitry.cheva@gmail.com>
2019-08-06 08:36:29 +0300

    loop 100 autogit bat

--- autogit.bat
+++ autogit.bat
@@ -1,4 +1,4 @@
-set datetimef=%date% at %time:~0,2%:%time:~3,2%:%time:~6,2%
+set datetimef = %date% at%time:~0,2%:%time:~3,2%:%time:~6,2%
 git add -A
 git commit -am "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 git pull
@@ -6,6 +6,7 @@ git push origin HEAD
 :loop
 IF /I "%1" == "loop" (
     ;timeout /t "%2"
+    set gitlog = ""
     git status --porcelain > git.log
     for /f %%i in (git.log) do set gitlog = "%gitlog% %%i "
     git add -A
