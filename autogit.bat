f9ce34e858ee6dd2b06121823fb18c89ae2bbffc
Dmitry Cheva <dmitry.cheva@gmail.com>
2019-08-06 08:58:47 +0300

    m

--- autogit.bat
+++ autogit.bat
@@ -1,16 +1,18 @@
 set datetimef = %date% at%time:~0,2%:%time:~3,2%:%time:~6,2%
 git add -A
-git commit -am "%1 %2 %3 %4 %5 %6 %7 %8 %9"
+set m = ""
+FOR %%a IN (%*) DO (
+  set m  = %m + a
+)
+git commit -am "%m"
 git pull
 git push origin HEAD
 :loop
 IF /I "%1" == "loop" (
-    set t = %2
-    IF /I %t% LEQ 60 do (set t=60)
     git add -A
     git commit -am "%datetimef%"
     git pull
     git push origin HEAD
-    timeout /t %t%
+    timeout /t "%2"
     goto loop
 )
