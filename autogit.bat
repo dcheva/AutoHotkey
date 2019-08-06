b97074058bc4b116d4c284b6ed7a43abcbf0ecd7
Dmitry Cheva <dmitry.cheva@gmail.com>
2019-08-06 07:29:11 +0300

    loop 100 autogit bat

---
+++ autogit.bat
@@ -0,0 +1,13 @@
+git add -A
+git commit -am "%1 %2 %3 %4 %5 %6 %7 %8 %9"
+git pull
+git push origin HEAD
+:loop
+IF /I "%1" == "loop" (
+	timeout /t "%1"
+	git add -A
+	git commit -am "%date% %time%"
+	git pull
+	git push origin HEAD
+	goto loop
+)
